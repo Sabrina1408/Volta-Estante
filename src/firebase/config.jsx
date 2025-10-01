dfc8c63f1863d78d5d44ef9c90a6243878599d9a
Sabrina Alves <sabrina.a.brito@gmail.com>
2025-10-01 19:56:01 -0300

    Hospedagem do site

---
+++ src/firebase/config.jsx
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXSy94BxVsUK_fa6kce-nE7D6M2sylQCM",
+  authDomain: "voltaaestante.firebaseapp.com",
+  projectId: "voltaaestante",
+  storageBucket: "voltaaestante.firebasestorage.app",
+  messagingSenderId: "330845862353",
+  appId: "1:330845862353:web:f937f589ab14fe6f9dc8f0",
+  measurementId: "G-15SHL658CD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app); // Banco de dados
+
+export {db};
